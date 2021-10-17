6114cd53f25abc2e4775385eeeea0fc323be487f
Sirin007 <dashkeivich@yandex.ru>
2021-10-17 15:51:11 +0300

    task 2

---
+++ decorator.py
@@ -0,0 +1,36 @@
+def deco(func):
+    def wrapper(*args):
+        for x in args:
+            print(x)
+        print(func(*args))
+    return wrapper
+
+
+@deco
+def a(start, finish):
+    return "result"
+
+
+a(1, 2)
+
+
+import time
+
+def deco_timing(func):
+
+    def wrapper(param,accuracy):
+        start = time.perf_counter()
+        func(param)
+        end = time.perf_counter()
+        print('Время выполнения: {} секунд.'.format(round(end-start,accuracy)))
+    return wrapper
+
+
+@deco_timing
+def dream(sec):
+    time.sleep(sec)
+
+dream(5,10)
+
+
+
